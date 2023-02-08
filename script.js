69ce7756ed8e801141cb0dd00edccf13eed05f9f
Alexandra July <catmon0201@gmail.com>
2023-02-08 19:28:31 +0200

    t

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //1
 
-for (var i=20; i <= 30; i=i+0.5) {
+for (var i=20; i <= 30; i += 0.5) {
     console.log(i) 
 }
 
 //2
 
 let n = 27;
-for (var i=10; i <= 100; i=i+10) {
+for (var i=10; i <= 100;  i += 10) {
     console.log(`${i*n} $`);
 }
 
@@ -39,12 +39,13 @@ console.log(isPrime(num));
 let number = Number(prompt("Введіть будьяке число"));
 
 function num(number) {
-    let res = Math.pow(number, 1/3);
-    console.log(res);
-    if(res !== 0){
-        return 'можна возвести в 3й ступінь';
-    }
-    return 'не можна возвести в 3й ступінь';
+    while (number % 3 == 0) {
+        number /= 3;
+        }
+        console.log(number == 1);
+        if(number == true){
+            return 'можна возвести в ступінь';
+        }
+        return 'не можна возвести в ступінь';
 }
-
 console.log(num(number));
